745522c97839c067906e3d0a4f19c654f2f02510
xAldox995 <valzanialdo@gmail.com>
2025-01-16 12:53:36 +0100

    end excercice 8

--- assets/js/D4.js
+++ assets/js/D4.js
@@ -36,9 +36,9 @@ console.log(crazySum(2, 2))
 const crazyDiff = function (n) {
     let result;
     if (n <= 19) {
-        result = Math.abs(n - 19) ;
+        result = Math.abs(n - 19);
     } else {
-        result = Math.abs (n - 19) * 3;
+        result = Math.abs(n - 19) * 3;
     }
     return result
 }
@@ -50,7 +50,16 @@ console.log(crazyDiff(18))
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+const boundary = function (n) {
+    let result
+    if (n >= 20 && n <= 100 || n === 400) {
+        result = true
+    } else {
+        result = false
+    }
+    return result
+}
+console.log(boundary(50))
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -58,26 +67,64 @@ console.log(crazyDiff(18))
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+const epify = function (string) {
+    let result;
+    let verifyString = 'Epicode '
+    if (string.startsWith(verifyString)) {
+        result = string
+    } else {
+        result = verifyString + string
+    }
+    return result
+}
+console.log(epify('Epicode'))
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const check3and7 = function (n){
+    let result;
+    if (n % 3 ===0 || n % 7 ===0){
+        console.log(`Il numero ${n} è un multiplo di 3 o 7`)
+        result = true;
+    }else{
+        console.log(`Il numero ${n} non è un multiplo di 3 o 7`)
+        result = false;
+    }
+    return result
+}
+console.log(check3and7(5))
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+const reverseString = function (string) {
+    let result = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string[i]
+    }
+    return result
+}
+console.log(reverseString('Epicode'))
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const upperFirst =  function(string){
+    let arrayOfString=[];
+    let stringSplit= string.split(' ');
+    for(let i=0; i<stringSplit.length; i++){
+        arrayOfString.push(stringSplit[i].charAt(0).toUpperCase() + stringSplit[i].slice(1))
+    }
+    return arrayOfString.join(' ')
+}
+console.log(upperFirst('ciao a tutti'))
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
